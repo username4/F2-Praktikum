eb917418acb380a6b2c86c7a4faf593f384a3e6b
username4 <micsverau@web.de>
2014-11-25 17:05:01 +0100

    BLM: a3 fast fertig

--- BLM/Auswertung/Messungen/mehrkanal/histogramm.py
+++ BLM/Auswertung/Messungen/mehrkanal/histogramm.py
@@ -5,7 +5,7 @@ import scipy.optimize as opt
 
 # plotte histogramm
 data = np.loadtxt("mehrkanal.DAT")
-hist, bins = np.histogram(data, bins=30, range = [data.min(),data.max()-5] )
+hist, bins = np.histogram(data, bins=30, range = [data.min(),data.max()-1] )
 
 # zeichne histogramm 
 
