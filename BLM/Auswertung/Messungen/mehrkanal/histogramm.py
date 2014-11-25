14c04aa3d4286696a2cf94505490b7b17b2adc0d
username4 <micsverau@web.de>
2014-11-25 15:56:10 +0100

    BLM: a2 hist erstellt

---
+++ BLM/Auswertung/Messungen/mehrkanal/histogramm.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2
+import numpy as np, matplotlib.pyplot as plt
+import scipy.optimize as opt 
+
+
+# plotte histogramm
+data = np.loadtxt("mehrkanal.DAT")
+hist, bins = np.histogram(data, bins=30, range = [data.min(),data.max()-5] )
+
+# zeichne histogramm 
+
+plt.grid(True) # zeichne koordinatengitter
+plt.xlabel(r"Strom (pA)")
+plt.ylabel(r"Haeufigkeit")
+
+plt.bar(bins[:-1],hist)
+
+
+plt.show()
