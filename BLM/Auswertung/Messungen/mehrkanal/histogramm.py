7ae5d013dba4c7cc46d7115b0ebbc6236a9016a1
Michael Eliachevitch <m.eliachevitch@gmx.net>
2014-12-15 11:53:30 +0100

    mehrkanal histogramm gefittet
    dazugehöriger plot geupdatet, fitwerte im plot

--- BLM/Auswertung/Messungen/mehrkanal/histogramm.py
+++ BLM/Auswertung/Messungen/mehrkanal/histogramm.py
@@ -2,22 +2,35 @@
 import numpy as np, matplotlib.pyplot as plt
 import scipy.optimize as opt 
 
+def gaus(x, mu, norm, sigma):
+    return  norm * 1./np.sqrt(2*np.pi*sigma**2) * np.exp(-1/2*(x-mu)**2/sigma**2)
+
 
 # plotte histogramm
 data = np.loadtxt("mehrkanal.DAT")
-hist, bins = np.histogram(data, bins=30, range = [data.min(),data.max()-1] )
+hist, bins = np.histogram(data, bins=200, range = [data.min(),data.max()-1] )
 
+fitbins1 = bins[(bins>10)*(bins<22)]
+fithist1 = hist[(bins>10)*(bins<22)]
+
+fitbins2 = bins[(bins>22)*(bins<26)]
+fithist2 = hist[(bins>22)*(bins<26)]
+
+p01 = [15., 800., 5.]
+p02 = [23., 100., 1.]
+
+popt1, pcov1 = opt.curve_fit(gaus, fitbins1, fithist1, p0=p01)
+popt2, pcov2 = opt.curve_fit(gaus, fitbins2, fithist2, p0=p02)
+
+plt.figure()
+plt.plot(fitbins1, gaus(fitbins1, popt1[0], popt1[1], popt1[2]), linewidth=1.5,
+         label="mean: (" + str(np.round(popt1[0],3))+r"$\pm$ "+str(np.round(pcov1[0,0],2))+") pA")
+plt.plot(fitbins2, gaus(fitbins2, popt2[0], popt2[1], popt2[2]), "r", linewidth=1.5,
+         label= "mean: (" + str(np.round(popt2[0],3))+r"$\pm$ "+str(np.round(pcov2[0,0],2)) +") pA")
+
+plt.legend(loc="best")
 # zeichne histogramm 
-plt.figure(1) # sowas wie die canvas bei root
-plt.grid()
-plt.xlabel(r"Strom (pA)")
-plt.ylabel(r"counts")
 
-plt.bar(bins[:-1],hist, # plt.bar benoetigt nur linke bingrenzen (bins[:-1]) und deren hoehen
-        width=(bins[1]-bins[0]), # nehme konstante binbreite an, dann haben alle bins die Breite vom ersten bin
-        color="#FFFFFF") 
-
-plt.figure(2)
 plt.grid()
 plt.xlabel(r"Strom (pA)")
 plt.ylabel(r"counts")
