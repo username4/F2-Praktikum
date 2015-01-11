0bb71d356911f8f7ac579425b38d66b609e4812d
Michael Eliachevitch <m.eliachevitch@gmx.net>
2015-01-11 21:02:05 +0100

    Einheiten in Zeitkalibrierungsplot nun in ns

--- Landefaktor/Auswertung/messungen/zeitkalibrierung.py
+++ Landefaktor/Auswertung/messungen/zeitkalibrierung.py
@@ -12,12 +12,13 @@ def linfunc(x, m, b):
 
 popt, pcov = curve_fit(linfunc, channels, times)
 
+plt.figure(1, [9,6])
 plt.plot(channels, times, "bo",
-         label="messwerte")
+         label="Messwerte")
 plt.plot(channels, linfunc(channels, popt[0], popt[1]), "r-",
-         label=r"linear fit: <time / channel> = ({0} $\pm$ {1}) s".format(np.round(popt[0],6),np.round(pcov[0,0],10)))
+         label=r"linear fit: <time / channel> = ({0} $\pm$ {1}) ns".format(np.round(popt[0],6),np.round(pcov[0,0],10)))
 plt.xlabel("channel #")
-plt.ylabel("time (s)")
+plt.ylabel("time (ns)")
 plt.legend(loc="best")
 plt.savefig("zeitkalibrierung.pdf")
 
